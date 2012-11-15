4dc302d50b14c2bf73284698ab41c4cde9264561
Michael Hanke <michael.hanke@gmail.com>
2012-11-15 11:09:15 +0100

    ENH: Basic project meta-data for distributing.

---
+++ setup.py
@@ -0,0 +1,59 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the vilay package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+""""""
+
+import os
+import sys
+from os.path import join as opj
+import vilay
+from distutils.core import setup
+from glob import glob
+
+
+__docformat__ = 'restructuredtext'
+
+extra_setuptools_args = {}
+if 'setuptools' in sys.modules:
+    extra_setuptools_args = dict(
+        tests_require=['nose'],
+        test_suite='nose.collector',
+        zip_safe=False
+    )
+
+def main(**extra_args):
+    setup(name         = 'vilay',
+          version      = vilay.__version__,
+          author       = 'Daniel Kottke and the vilay developers',
+          author_email = 'daniel@kottke.eu',
+          license      = 'GPL',
+          url          = 'https://github.com/dakot/vilay',
+          download_url = 'https://github.com/dakot/vilay/tags',
+          description  = '',
+          long_description = open('README.rst').read(),
+          classifiers  = ["Development Status :: 3 - Alpha",
+                          "Environment :: GUI",
+                          "Intended Audience :: Science/Research",
+                          "License :: OSI Approved :: GPL",
+                          "Operating System :: OS Independent",
+                          "Programming Language :: Python",
+                          "Topic :: Scientific/Engineering"],
+          platforms    = "OS Independent",
+          provides     = ['vilay'],
+          # please maintain alphanumeric order
+          packages     = [ 'vilay',
+                           'vilay.qt_ui',
+                           ],
+          package_data = {
+              'vilay': ['icons/*']
+            },
+          scripts      = glob(os.path.join('bin', '*'))
+          )
+
+if __name__ == "__main__":
+    main(**extra_setuptools_args)
